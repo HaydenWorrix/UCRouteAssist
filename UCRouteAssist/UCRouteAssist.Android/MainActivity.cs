f1184c68ecdff8554f5c8c153c7c63f683f13393
Adam Ralston <aralston.12@gmail.com>
2019-12-09 19:19:34 -0500

    Changes so far. Code is broken for now. Reformatting to better fit the Xamarin framework is needed.

--- UCRouteAssist/UCRouteAssist.Android/MainActivity.cs
+++ UCRouteAssist/UCRouteAssist.Android/MainActivity.cs
@@ -1,26 +1,51 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Fragment = Android.Support.V4.App.FragmentActivity;
+using Android.Content;
+using Android.Support.V7.App;
 
 namespace UCRouteAssist.Droid
 {
     [Activity(Label = "UCRouteAssist", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+
     {
+        private GoogleMap googleMap;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.map_layout);
-            LoadApplication(new App());
+            //global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            //global::Xamarin.FormsMaps.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.main_activity_layout);
+            if (savedInstanceState == null)
+            {
+                SupportMapFragment supportMapFragment = (SupportMapFragment)SupportFragmentManager
+               .FindFragmentById(Resource.Id.map);
+                supportMapFragment.GetMapAsync();
+            }
+
+            
+            //Intent intent = new Intent(this, typeof(MapFragmentActivity));
+            //StartActivity(intent);
+
+
+            //LoadApplication(new App());
+        }
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            this.googleMap = googleMap;
+            LatLng cincinnati = new LatLng(39, -84);
+            this.googleMap.AddMarker(new MarkerOptions().SetPosition(cincinnati).SetTitle("Test"));
+            this.googleMap.MoveCamera(CameraUpdateFactory.NewLatLng(cincinnati));
         }
     }
 }
---
+++ UCRouteAssist/UCRouteAssist.Android/MapFrag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace UCRouteAssist.Droid
+{
+    class MapFrag : Fragment
+    {
+        private GoogleMap googleMap;
+        
+        /*FragmentActivity listener;
+
+        public void override OnAttach(Context context)
+        {
+            base.OnAttach(context);
+            if (context is Activity)
+            {
+                this.listener = (FragmentActivity)context;
+
+            }
+
+        }
+        */
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(Resource.Layout.map_layout, parent, false);
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+           
+        }
+
+        
+    }
+}
